e9d766e086a27d8beb8bd6356976e4a906225e37
Abdul Samad Ansari <hello@abdulsamad.dev>
2024-08-29 03:54:00 +0530

    chore: inti fill form from popup button

--- apps/extension/src/manifest.ts
+++ apps/extension/src/manifest.ts
@@ -14,7 +14,7 @@ export default defineManifest({
     128: 'img/logo-128.png',
   },
   action: {
-    // default_popup: 'popup.html',
+    default_popup: 'popup.html',
     default_icon: 'img/logo-48.png',
   },
   // options_page: 'options.html',
@@ -26,7 +26,7 @@ export default defineManifest({
     {
       matches: ['http://*/*', 'https://*/*'],
       js: ['src/contentScript/index.ts'],
-      // all_frames: true,
+      all_frames: true,
     },
   ],
   side_panel: {
--- apps/extension/src/popup/Popup.tsx
+++ apps/extension/src/popup/Popup.tsx
@@ -1,29 +1,26 @@
-import { useEffect, useState } from 'react'
-
 import { Button } from '@/components/ui/button'
 
+import { getCurrentTab } from '@/utils'
+import { MESSAGES } from '@/consts'
+
 export const Popup = () => {
-  const [tabId, setTabId] = useState<number>()
-  const [countSync, setCountSync] = useState(0)
+  const fillAllForms = async () => {
+    const tab = await getCurrentTab()
 
-  useEffect(() => {
-    ;(async () => {
-      let queryOptions = { active: true, lastFocusedWindow: true }
-      let [tab] = await chrome.tabs.query(queryOptions)
+    if (!tab.id) return null
 
-      setTabId(tab?.id)
-    })()
-  }, [])
+    const { INIT_AUTOFILL_ALL } = MESSAGES
+
+    chrome.tabs.sendMessage(tab.id, INIT_AUTOFILL_ALL)
+  }
 
   return (
-    <div className="relative w-[400px] h-[600px] bg-gray-100 p-4 box-border">
-      <h1 className="text-xl font-semibold mb-4">Popup</h1>
-      <p className="text-gray-700 mb-4">
-        This is your popup content. You can add more components as needed.
-      </p>
-      <div className="flex gap-2 items-center justify-center flex-col">
-        <div className="y-5 text-xl">{countSync}</div>
-        <Button onClick={() => setCountSync((c) => c + 1)}>Click Me</Button>
+    <div className="relative w-[250px] h-[400px] bg-gray-100 p-4 box-border">
+      <div className="h-full w-full">
+        <h1 className="text-xl font-semibold mb-4">Popup</h1>
+        <div className="flex gap-2 items-center justify-center flex-col">
+          <Button onClick={fillAllForms}>Fill All Forms</Button>
+        </div>
       </div>
     </div>
   )
