b5bef89a9457d239b540d6295902d948a9d8610c
Abdul Samad Ansari <hello@abdulsamad.dev>
2024-09-01 20:26:58 +0530

    chore: error message for internal page

--- apps/extension/src/popup/Popup.tsx
+++ apps/extension/src/popup/Popup.tsx
@@ -34,17 +34,28 @@ export const Popup = () => {
   }
 
   return (
-    <div className="relative w-[250px] h-[400px] bg-gray-100 p-4 box-border">
+    <div className="relative w-[250px] h-[400px] bg-gray-100 p-4 box-border ">
       <div className="h-full w-full">
         <h1 className="text-xl font-semibold mb-4">Formatic</h1>
         <div className="flex gap-2 items-center justify-center flex-col mt-4">
-          <div className="flex flex-col gap-2">
-            <Button onClick={fillAllForms} disabled={isDisabled || isAutofilling}>
-              {isAutofilling ? 'Autofilling...' : 'Fill All Forms'}
-            </Button>
+          <div className="flex flex-col justify-between h-full">
+            <div className="flex flex-col gap-2">
+              <Button onClick={fillAllForms} disabled={isDisabled || isAutofilling}>
+                {isAutofilling ? 'Autofilling...' : 'Fill All Forms'}
+              </Button>
+            </div>
           </div>
         </div>
       </div>
+      {isDisabled && (
+        <div className="absolute bottom-0 left-0 right-0 p-4">
+          <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 relative rounded-lg" role="alert">
+            <span className="block sm:inline">
+              You are on an internal page. Please visit a website to use Formatic.
+            </span>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
