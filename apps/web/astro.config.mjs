671199408dab84a3612f3f91aabc02667a326ff4
Abdul Samad Ansari <hello@abdulsamad.dev>
2024-10-01 10:38:23 +0530

    refactor: added astro react

--- apps/web/astro.config.mjs
+++ apps/web/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from "astro/config";
+import react from "@astrojs/react";
 
 export default defineConfig({
   output: "static",
   server: {
     port: 3000,
   },
+  integrations: [react()],
 });
