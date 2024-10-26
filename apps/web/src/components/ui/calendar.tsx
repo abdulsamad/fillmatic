2a1eb57a6b7b8b153408fe111232b6a07628da4b
Abdul Samad Ansari <hello@abdulsamad.dev>
2024-10-27 01:29:47 +0530

    refactor: fix lint errors

--- apps/web/src/components/ui/calendar.tsx
+++ apps/web/src/components/ui/calendar.tsx
@@ -60,7 +60,7 @@ function Calendar({
       components={{
         IconLeft: ({ ...props }) => <ChevronLeftIcon className="h-4 w-4" />,
         IconRight: ({ ...props }) => <ChevronRightIcon className="h-4 w-4" />,
-      }}
+      } as any}
       {...props}
     />
   )
